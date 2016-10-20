002fc9ba3a446544ee28b05d7c23a38076e666a6
hazrmard <ibrahim78786@gmail.com>
2016-10-20 15:17:54 -0500

    fixed english block bug

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Urdown
-An Urdu markdown editor | انٹرنیٹ پر مبنی ٹیکسٹ ایڈیٹر ہے۔
+An Urdu markdown editor | انٹرنیٹ پر مبنی ٹیکسٹ ایڈیٹر
 
 Live demo at [https://hazrmard.github.io/Urdown](https://hazrmard.github.io/Urdown).  
-  
-Urdown (Udru + Markdown) is an online markdown editor with preference for right-to-left 
-languages like Urdu. It can embed blocks of left-to-right languages like English by 
+
+Urdown (Udru + Markdown) is an online markdown editor with preference for right-to-left
+languages like Urdu. It can embed blocks of left-to-right languages like English by
 enclosing them in three commas `,,,`.
--- static/js/urdown.js
+++ static/js/urdown.js
@@ -1,15 +1,19 @@
 // This sets up an extension object to be passed to showdown.
 // The englishBlock extension sets the direction of text within to left-to-right
-var englishBlock = function () {
-  var myext1 = {
-    type: 'lang',
-    regex: /[\n\r]+[,|،]{3}([\s\S]+?)[,|،]{3}[\s]*?/gm,
-    replace: '<div dir="ltr" class="ltr-div">$1</div>'
-  };
-  return [myext1];
+var englishBlock = function (sd) {
+    return function() {
+        var myext1 = {
+          type: 'lang',
+          regex: /[\n\r]+?[,|،]{3}([\s\S]+?)[\n\r]+[,|،]{3}[\s]+?/gm,
+          replace: function(match, capture) {
+              return '\n<div dir="ltr" class="ltr-div">'+sd.makeHtml(capture)+'</div>\n';
+          }
+        };
+        return [myext1];
+    }
 };
 
-showdown.extensions.englishblock = englishBlock
+showdown.extensions.englishblock = englishBlock(new showdown.Converter())
 
 // setting up the main angular app
 var urdown = angular.module('Urdown', ['ng-showdown'])
